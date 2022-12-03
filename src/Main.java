f6cededbe04607594944a1f2079a6db9b921ab80
Администратор <Timoshenko311989@gmail.com>
2022-12-03 17:36:59 +0300

    Start_Cycles_2(Циклы_Часть_2)

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         //Задание 1
         System.out.println("Задание 1");
         int accumulation = 15000;
-        double sumPercent = 0;
+        int sumPercent = 0;
         double percent = 0.01;
         int i = 0;
         while (sumPercent < 2_459_000){
             i++;
-            sumPercent = (sumPercent + accumulation) * (1 + percent);
+            sumPercent = (int) ((sumPercent + accumulation) * (1 + percent));
             System.out.println("Месяц " + i + ", сумма накоплений равна " + sumPercent + " рублей");
         }
 
@@ -40,22 +40,22 @@ public class Main {
 
         //Задание 4
         System.out.println("Задание 4");
+        int fullSum = 12_000_000;
+        int monthSum = 0;
         int accumulation2 = 15000;
-        float sumPercent2 = 0;
-        float percent2 = 0.07f;
-        int i2 = 0;
-        while (sumPercent2 < 12_000_000){
-                i2++;
-            sumPercent2 = (sumPercent2 + accumulation2) * ( 1 + percent2);
-            System.out.println("Месяц " + i2 + ", сумма накоплений равна " + sumPercent2 + " рублей");
+        double percent2 = 0.07;
+        for (; accumulation2 < fullSum; monthSum++){
+            accumulation2+=accumulation2*percent2;
+            System.out.println("Месяц " + monthSum + ", сумма сумма накоплений равна " + accumulation2 + " рублей");
+
         }
 
         //Задание 5
         System.out.println("Задание 5");
-        for (i2 = 6 ;i2<100; i2+=6){
-            System.out.println(i2);
+        for (monthSum = 6; monthSum <100; monthSum +=6){
+            accumulation2*=6;
+                System.out.println("Месяц " + monthSum + ", сумма сумма накоплений равна " + accumulation2 + " рублей");
         }
-
 
     }
 }
